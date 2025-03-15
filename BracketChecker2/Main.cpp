209910c5db0dc273759cc45b8f64c73c949aee06
overeactor <alireza_bi965@yahoo.com>
2025-03-15 20:52:04 +0300

    Main.cpp is prepared for doxygen

--- BracketChecker2/Main.cpp
+++ BracketChecker2/Main.cpp
@@ -1,4 +1,21 @@
+/**
+ * @file Main.cpp
+ * @brief Entry point for the bracket checking application.
+ *
+ * This program creates an instance of the BracketChecker2 class
+ * and checks the brackets in a specified file.
+ */
+
 #include "BracketChecker2.h"
+
+ /**
+  * @brief Main function of the program.
+  *
+  * Creates an instance of BracketChecker2 and checks the brackets
+  * in the file "code.txt".
+  *
+  * @return int Returns 0 upon successful execution.
+  */
 
 int main() {
     BracketChecker2 checker;
