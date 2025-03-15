22a05c018a131f32b8b329cec5a7827fb8a1fa17
overeactor <alireza_bi965@yahoo.com>
2025-03-15 20:48:37 +0300

    header file is prepared for doxygen

--- BracketChecker2/BracketChecker2.h
+++ BracketChecker2/BracketChecker2.h
@@ -1,3 +1,9 @@
+/**
+ * @file BracketChecker2.h
+ * @brief Header file for the BracketChecker2 class.
+ *
+ * This class provides functionality to check for balanced brackets in a given file.
+ */
 #pragma once
 #ifndef BRACKETCHECKER2_H  
 #define BRACKETCHECKER2_H
@@ -10,13 +16,42 @@
 
 using namespace std;
 
+/**
+ * @class BracketChecker2
+ * @brief A class to check for balanced brackets in a file.
+ */
 class BracketChecker2 {
 public:
+	/**
+	 * @brief Constructor for BracketChecker2.
+	 */
 	BracketChecker2();
+	
+	/**
+	 * @brief Checks the brackets in the given file.
+	 * @param filename The name of the file to check.
+	 */
 	void checkBrackets(const string& filename);
+
 private:
+	/**
+	* @brief Checks if a character is an opening bracket.
+	* @param ch Character to check.
+	* @return True if opening bracket, false otherwise.
+	*/
 	bool isOpeningBracket(char ch);
+	/**
+	 * @brief Checks if a character is a closing bracket.
+	 * @param ch Character to check.
+	 * @return True if closing bracket, false otherwise.
+	 */
 	bool isClosingBracket(char ch);
+	/**
+	 * @brief Checks if two characters are matching bracket pairs.
+	 * @param open Opening bracket character.
+	 * @param close Closing bracket character.
+	 * @return True if they match, false otherwise.
+	 */
 	bool isMatchingPair(char open, char close);
 
 };
@@ -24,4 +59,4 @@ private:
 
 
 
-#endif
+#endif // BRACKETCHECKER2_H
