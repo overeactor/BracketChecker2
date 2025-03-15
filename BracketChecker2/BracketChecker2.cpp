c89e41795f47d16a00f3a548b797c364d549ce42
overeactor <alireza_bi965@yahoo.com>
2025-03-15 18:57:57 +0300

    the function for matching brackets added

--- BracketChecker2/BracketChecker2.cpp
+++ BracketChecker2/BracketChecker2.cpp
@@ -9,3 +9,9 @@ bool BracketChecker2::isOpeningBracket(char ch) {
 bool BracketChecker2::isClosingBracket(char ch) {
     return ch == ')' || ch == ']' || ch == '}';
 }
+
+bool BracketChecker2::isMatchingPair(char open, char close) {
+    return (open == '(' && close == ')') ||
+        (open == '[' && close == ']') ||
+        (open == '{' && close == '}');
+}
