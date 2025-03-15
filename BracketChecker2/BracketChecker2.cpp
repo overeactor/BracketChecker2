dd9f4f75abe4defd03d7f4e51a44cefdb0cb02ad
overeactor <alireza_bi965@yahoo.com>
2025-03-15 18:55:20 +0300

    the function for closing bracket added

--- BracketChecker2/BracketChecker2.cpp
+++ BracketChecker2/BracketChecker2.cpp
@@ -5,3 +5,7 @@ BracketChecker2::BracketChecker2() {}
 bool BracketChecker2::isOpeningBracket(char ch) {
     return ch == '(' || ch == '[' || ch == '{';
 }
+
+bool BracketChecker2::isClosingBracket(char ch) {
+    return ch == ')' || ch == ']' || ch == '}';
+}
