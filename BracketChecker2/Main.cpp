cf806d48cc09575b234942a51f7f0c009f515c3f
overeactor <alireza_bi965@yahoo.com>
2025-03-18 23:08:08 +0300

    got rid of the old functions in main to rebuild

--- BracketChecker2/Main.cpp
+++ BracketChecker2/Main.cpp
@@ -1,24 +1,7 @@
-/**
- * @file Main.cpp
- * @brief Entry point for the bracket checking application.
- *
- * This program creates an instance of the BracketChecker2 class
- * and checks the brackets in a specified file.
- */
-
 #include "BracketChecker2.h"
 
- /**
-  * @brief Main function of the program.
-  *
-  * Creates an instance of BracketChecker2 and checks the brackets
-  * in the file "code.txt".
-  *
-  * @return int Returns 0 upon successful execution.
-  */
+ 
 
 int main() {
-    BracketChecker2 checker;
-    checker.checkBrackets("code.txt");
-    return 0;
+   
 }
