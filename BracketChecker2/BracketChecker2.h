72dae309ab0f1085187a8d982d38a17b0fa2ca78
overeactor <alireza_bi965@yahoo.com>
2025-03-23 23:25:57 +0300

    header file prepared for doxygen

--- BracketChecker2/BracketChecker2.h
+++ BracketChecker2/BracketChecker2.h
@@ -1,3 +1,13 @@
+/**
+ * @file BracketChecker2.h
+ * @brief Header file for bracket validation functions.
+ *
+ * This file contains function declarations for checking matching brackets
+ * in a given input file. It includes utility functions to determine bracket types
+ * and handle file input/output operations.
+ */
+
+
 #pragma once
 #ifndef BRACKETCHECKER2_H  
 #define BRACKETCHECKER2_H
@@ -8,15 +18,56 @@
 #include <string>
 #include <vector>
 
+
+ /**
+  * @brief Checks if the given character is an opening bracket.
+  *
+  * @param ch The character to check.
+  * @return True if it is an opening bracket, otherwise false.
+  */
 using namespace std;
 
 
+/**
+ * @brief Checks if the given character is an opening bracket.
+ *
+ * @param ch The character to check.
+ * @return True if it is an opening bracket, otherwise false.
+ */
 bool isOpeningBracket(char ch);
+
+
+/**
+ * @brief Checks if the given character is a closing bracket.
+ *
+ * @param ch The character to check.
+ * @return True if it is a closing bracket, otherwise false.
+ */
 bool isClosingBracket(char ch);
+
+/**
+ * @brief Checks if the given pair of brackets match.
+ *
+ * @param open The opening bracket character.
+ * @param close The closing bracket character.
+ * @return True if they form a valid pair, otherwise false.
+ */
 bool isMatchingPair(char open, char close);
 
-
+/**
+ * @brief Reads the input file and finds unmatched brackets.
+ *
+ * @param filename The name of the file to read.
+ * @return A vector containing unmatched brackets with their positions.
+ */
 vector<pair<char, pair<int, int>>> read_input_file(const string& filename);
+
+/**
+ * @brief Outputs the result of bracket matching to a file.
+ *
+ * @param outputFilename The name of the output file.
+ * @param errors A vector containing unmatched brackets with their positions.
+ */
 void print_result(const string& outputFilename, const vector<pair<char, pair<int, int>>>& errors);
 
 
