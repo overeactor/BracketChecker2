d1a85b44e54ef27cc189d5f6c7a5027bc87724dc
overeactor <alireza_bi965@yahoo.com>
2025-03-23 23:09:21 +0300

    html files from doxygen removed

--- html/annotated_dup.js
+++
@@ -1,4 +0,0 @@
-var annotated_dup =
-[
-    [ "BracketChecker2", "class_bracket_checker2.html", "class_bracket_checker2" ]
-];
--- html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['bracketchecker2_0',['BracketChecker2',['../class_bracket_checker2.html',1,'']]]
-];
