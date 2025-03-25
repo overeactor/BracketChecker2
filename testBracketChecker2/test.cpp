0093d8929db2ff378880682f87f49266508a0835
overeactor <alireza_bi965@yahoo.com>
2025-03-25 12:40:20 +0300

    initial tests added

---
+++ testBracketChecker2/test.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+#include "../BracketChecker2/BracketChecker2.h"  // Adjust the path if needed
+#include "../BracketChecker2/BracketChecker2.cpp"  // Adjust the path if needed
+
+// Test suite name updated to match file name
+TEST(testBracketChecker2, IsOpeningBracket) {
+    EXPECT_TRUE(isOpeningBracket('('));
+    EXPECT_TRUE(isOpeningBracket('{'));
+    EXPECT_TRUE(isOpeningBracket('['));
+    EXPECT_FALSE(isOpeningBracket('x'));
+    EXPECT_FALSE(isOpeningBracket(')'));
+}
+
+TEST(testBracketChecker2, IsClosingBracket) {
+    EXPECT_TRUE(isClosingBracket(')'));
+    EXPECT_TRUE(isClosingBracket('}'));
+    EXPECT_TRUE(isClosingBracket(']'));
+    EXPECT_FALSE(isClosingBracket('('));
+    EXPECT_FALSE(isClosingBracket('a'));
+}
+
+TEST(testBracketChecker2, IsMatchingPair) {
+    EXPECT_TRUE(isMatchingPair('(', ')'));
+    EXPECT_TRUE(isMatchingPair('[', ']'));
+    EXPECT_TRUE(isMatchingPair('{', '}'));
+    EXPECT_FALSE(isMatchingPair('(', ']'));
+    EXPECT_FALSE(isMatchingPair('{', ')'));
+}
+
+// Run all tests
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
