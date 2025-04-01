1872ef6f48738e5c6ee9383b33ddc1cc41eb36fe
overeactor <alireza_bi965@yahoo.com>
2025-04-01 17:25:08 +0300

    html file changed for adding graphs to doxyfile

---
+++ BracketChecker2/html/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['bracket_20checker_20documentation_0',['Bracket Checker Documentation',['../index.html',1,'']]],
+  ['bracketchecker2_2ecpp_1',['BracketChecker2.cpp',['../_bracket_checker2_8cpp.html',1,'']]],
+  ['bracketchecker2_2eh_2',['BracketChecker2.h',['../_bracket_checker2_8h.html',1,'']]]
+];
---
+++ BracketChecker2/html/search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['bracketchecker2_2ecpp_0',['BracketChecker2.cpp',['../_bracket_checker2_8cpp.html',1,'']]],
+  ['bracketchecker2_2eh_1',['BracketChecker2.h',['../_bracket_checker2_8h.html',1,'']]]
+];
