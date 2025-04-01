ef405b05e76c6d5b632c9cdfab484eee0bf76d56
overeactor <alireza_bi965@yahoo.com>
2025-04-01 20:03:32 +0300

    changing read_input_file function so removed its code

--- BracketChecker2/BracketChecker2.cpp
+++ BracketChecker2/BracketChecker2.cpp
@@ -25,67 +25,8 @@ bool isMatchingPair(char open, char close) {
 
 
 
-vector<pair<char, pair<int, int>>> read_input_file(const string& filename) {
-    ifstream file(filename);
-    if (!file) {
-        cerr << "Error: Cannot open file " << filename << endl;
-        return {};
-    }
+vector<pair<char, pair<int, int>>> read_input_file(const string& filename) 
 
-
-    
-    stack<pair<char, pair<int, int>>> bracketStack;
-    vector<pair<char, pair<int, int>>> errorPositions;
-    string line;
-    int lineNum = 0;
-    bool inBlockComment = false;
-
-    
-    
-    while (getline(file, line)) {
-        lineNum++;
-        bool inLineComment = false;
-
-        for (size_t i = 0; i < line.size(); i++) {
-            char ch = line[i];
-
-            // Handle comments correctly
-            if (i < line.size() - 1) {
-                if (!inBlockComment && line[i] == '/' && line[i + 1] == '/') {
-                    inLineComment = true;
-                }
-                else if (!inLineComment && !inBlockComment && line[i] == '/' && line[i + 1] == '*') {
-                    inBlockComment = true;
-                    i++; // Skip next character
-                    continue;
-                }
-                else if (inBlockComment && line[i] == '*' && line[i + 1] == '/') {
-                    inBlockComment = false;
-                    i++; // Skip next character
-                    continue;
-                }
-            }
-
-
-            // Ensure brackets inside both block and line comments are ignored completely
-            if (inBlockComment || inLineComment) {
-                continue;
-            }
-
-            // Process brackets only when NOT inside any type of comment
-            if (isOpeningBracket(ch)) {
-                bracketStack.push({ ch, {lineNum, i + 1} });
-            }
-            else if (isClosingBracket(ch)) {
-                if (!bracketStack.empty() && isMatchingPair(bracketStack.top().first, ch)) {
-                    bracketStack.pop();
-                }
-                else {
-                    errorPositions.push_back({ ch, {lineNum, i + 1} });
-                }
-            }
-        }
-    }
            
 
     while (!bracketStack.empty()) {
