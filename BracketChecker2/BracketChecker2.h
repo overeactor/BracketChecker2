1b9f6c1a22576f806af44055ea844e33ca833d0c
overeactor <alireza_bi965@yahoo.com>
2025-04-01 21:29:38 +0300

    doxygen comment added for parse brackets function in header

--- BracketChecker2/BracketChecker2.h
+++ BracketChecker2/BracketChecker2.h
@@ -64,6 +64,12 @@ bool isMatchingPair(char open, char close);
 vector<string> read_input_file(const string& filename);
 
 
+/**
+ * @brief Parses the given lines of text to detect unmatched brackets.
+ *
+ * @param lines A vector of strings representing the lines of code.
+ * @return A vector of unmatched brackets with their positions (line and column).
+ */
 vector<pair<char, pair<int, int>>> parse_brackets(const vector<string>& lines);
 
 /**
