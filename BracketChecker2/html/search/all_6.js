d0945d60bc6a010fd9797c7b6c2ee7f0d7e329bd
overeactor <alireza_bi965@yahoo.com>
2025-04-01 21:42:01 +0300

    html file modified for new doxygen changes

--- BracketChecker2/html/search/all_6.js
+++ BracketChecker2/html/search/all_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['print_5fresult_0',['print_result',['../_bracket_checker2_8cpp.html#ae0e7a8f1250bac2b529e0fa6c9184e91',1,'print_result(const string &amp;outputFilename, const vector&lt; pair&lt; char, pair&lt; int, int &gt; &gt; &gt; &amp;errors):&#160;BracketChecker2.cpp'],['../_bracket_checker2_8h.html#ae0e7a8f1250bac2b529e0fa6c9184e91',1,'print_result(const string &amp;outputFilename, const vector&lt; pair&lt; char, pair&lt; int, int &gt; &gt; &gt; &amp;errors):&#160;BracketChecker2.cpp']]]
+  ['parse_5fbrackets_0',['parse_brackets',['../_bracket_checker2_8cpp.html#abb71e368f0ecfe54b8ea3335a2905143',1,'parse_brackets(const vector&lt; string &gt; &amp;lines):&#160;BracketChecker2.cpp'],['../_bracket_checker2_8h.html#abb71e368f0ecfe54b8ea3335a2905143',1,'parse_brackets(const vector&lt; string &gt; &amp;lines):&#160;BracketChecker2.cpp']]],
+  ['print_5fresult_1',['print_result',['../_bracket_checker2_8cpp.html#ae0e7a8f1250bac2b529e0fa6c9184e91',1,'print_result(const string &amp;outputFilename, const vector&lt; pair&lt; char, pair&lt; int, int &gt; &gt; &gt; &amp;errors):&#160;BracketChecker2.cpp'],['../_bracket_checker2_8h.html#ae0e7a8f1250bac2b529e0fa6c9184e91',1,'print_result(const string &amp;outputFilename, const vector&lt; pair&lt; char, pair&lt; int, int &gt; &gt; &gt; &amp;errors):&#160;BracketChecker2.cpp']]]
 ];
