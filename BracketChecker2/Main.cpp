8472cc0244ac9e54d80081fd234f68180d3046ff
overeactor <alireza_bi965@yahoo.com>
2025-04-02 13:48:54 +0300

    print result in main function fixed

--- BracketChecker2/Main.cpp
+++ BracketChecker2/Main.cpp
@@ -20,6 +20,7 @@ int main() {
 	vector<string> lines = read_input_file(inputFile);
 	vector<pair<char, pair<int, int>>> errors = parse_brackets(lines);
 
+	print_result(outputFile, errors);
 
 	cout << "Bracket checking complete. Results saved to " << outputFile << endl;
 
