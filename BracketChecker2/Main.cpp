248534d32f68fa54369ad72637ab4c6e8f65f926
overeactor <alireza_bi965@yahoo.com>
2025-04-06 21:43:43 +0300

    doxygen format deleted for further modification on code

--- BracketChecker2/Main.cpp
+++ BracketChecker2/Main.cpp
@@ -1,18 +1,8 @@
-/**
- * @file Main.cpp
- * @brief Main entry point for the Bracket Checker program.
- */
+
 
 #include "BracketChecker2.h"
 
  
- /**
-  * @brief The main function of the program.
-  *
-  * this function Reads an input file, parses it for unmatched brackets, and writes the results to an output file.
-  *
-  * @return 0 on successful execution.
-  */
 int main() {
 	string inputFile = "code.txt";
 	string outputFile = "result.txt";
@@ -30,19 +20,3 @@ int main() {
 }
 
 
-/**
- * @mainpage Bracket Checker Documentation
- *
- * @section intro Introduction
- * This project is a simple bracket validation tool that scans a text file
- * and verifies that all brackets (parentheses, square brackets, and curly braces) are correctly matched.
- *
- * @section usage Usage
- * - The program reads an input file containing code or text.
- * - It detects any unmatched opening or closing brackets.
- * - The results are written to an output file.
- *
- * @section author Author
- * Created by [Bebahani asl A]
- *
- */
