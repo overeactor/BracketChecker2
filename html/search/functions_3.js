c86988aa19cd0ba6827c2a6c4c876b58f4582068
overeactor <alireza_bi965@yahoo.com>
2025-04-06 23:40:24 +0300

    old html doxygen file deleted

--- html/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['read_5finput_5ffile_0',['read_input_file',['../_bracket_checker2_8cpp.html#a5e7b803db2016b273a77cdff3c3d0332',1,'read_input_file(const string &amp;filename):&#160;BracketChecker2.cpp'],['../_bracket_checker2_8h.html#a5e7b803db2016b273a77cdff3c3d0332',1,'read_input_file(const string &amp;filename):&#160;BracketChecker2.cpp']]]
-];
--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['bracket_20checker_20documentation_0',['Bracket Checker Documentation',['../index.html',1,'']]]
-];
